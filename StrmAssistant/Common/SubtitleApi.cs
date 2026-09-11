f2f6938980a99d752b14e343c87cad284e04c7e5
huhengbo <huhbo1992@126.com>
2026-09-11 21:12:31 +0800

    fix: restore LINQ import after subtitle resolver refactor

--- StrmAssistant/Common/SubtitleApi.cs
+++ StrmAssistant/Common/SubtitleApi.cs
@@ -12,6 +12,7 @@ using MediaBrowser.Model.MediaInfo;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
